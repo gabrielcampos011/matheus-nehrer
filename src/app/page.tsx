f4bf3c2461252971b34c562aef48323d56a0ef43
gabrielcampos011 <gabrielcamposra@gmail.com>
2026-06-04 19:21:34 -0300

    feat: assemble Home page with all sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
 import { Hero } from '@/components/sections/Hero'
 import { Manifesto } from '@/components/sections/Manifesto'
+import { Procedimentos } from '@/components/sections/Procedimentos'
+import { Trabalhos } from '@/components/sections/Trabalhos'
+import { Depoimentos } from '@/components/sections/Depoimentos'
+import { Clinicas } from '@/components/sections/Clinicas'
+import { FAQ } from '@/components/sections/FAQ'
+import { CTAFinal } from '@/components/sections/CTAFinal'
 
 export default function Home() {
   return (
     <main className="pt-16">
       <Hero />
       <Manifesto />
+      <Procedimentos />
+      <Trabalhos />
+      <Depoimentos />
+      <Clinicas />
+      <FAQ />
+      <CTAFinal />
     </main>
   )
 }
